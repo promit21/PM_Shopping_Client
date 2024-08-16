8a02646f24a9ce68800318aa9c23c81c145b48ef
Promit Mondol <promitmondol21@gmail.com>
2024-08-16 19:54:33 +0600

    Code edit

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,15 +2,16 @@ import { Outlet } from "react-router-dom";
 import Footer from "../Pages/Shared/Footer/Footer";
 import Navbar from "../Pages/Shared/Navbar/Navbar";
 
-
 const Main = () => {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div>
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Main;
